c0079387cee155f0b09425a54ae6fb2446451585
Daniela <danielaandrade66@gmail.com>
2025-03-03 11:24:47 -0600

    Agrega la página de contacto.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'about-me', component: AboutMeComponent },
+    { path: 'contact', component: ContactComponent },
 
 ];
