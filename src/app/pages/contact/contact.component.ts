d6d2a86ec7e448dcf25effdea186d718ede3555f
Daniela <danielaandrade66@gmail.com>
2025-03-03 12:55:34 -0600

    Cambia el favicon por el del proyecto. Agrega la versión mobile del form, incluye prueba con correo.

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  public sendEmail(e: Event) {
+    e.preventDefault();
 
+    emailjs
+      .sendForm('service_muqbgll', 'template_mensajeNuevo', e.target as HTMLFormElement, {
+        publicKey: 'waQBOhbn0BCA5GIrA',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert('Correo enviado con éxito 📩');
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+          alert('Error al enviar el correo ❌');
+        },
+      );
+  }
 }
