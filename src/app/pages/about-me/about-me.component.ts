8efc430c50be6cad3760ca19abe863fa565d9a89
Daniela <danielaandrade66@gmail.com>
2025-03-03 13:32:21 -0600

    Agrega vistas responsive. JS de boostrap y completa header.

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-about-me',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
